b74bfce78bdf036bce888b35cfb1b7faf2a1ea55
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-09 22:37:35 +0500

    install next

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['encrypted-tbn0.gstatic.com', 'upload.wikimedia.org', 'i.pinimg.com'],
   },
